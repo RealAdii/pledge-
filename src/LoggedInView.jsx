81e3176d49c0b8bdc5a396860d17b290e19fbfc3
Adithya Dinesh <adithyadinesh@Adithyas-MacBook-Air.local>
2025-01-07 12:25:44 +0530

    loading component added

---
+++ src/LoggedInView.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function LoggedInView() {
+  return (
+    <div className="app">
+      <div className="postcontent">
+        <h1 className="sub-title">
+          Pledge today,<br />
+          Prove tomorrow,<br />
+          Profit always
+        </h1>
+      </div>
+    </div>
+  );
+} 
